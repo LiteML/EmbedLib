20f9996e831ffe65e5f21b3e251fe1a4cb75d456
chris <rihan.chen@tendcloud.com>
2017-08-18 16:48:43 +0800

    model

---
+++ src/main/scala/embed/lda/model/MPLDAModel.scala
@@ -0,0 +1,10 @@
+package embed.lda.model
+
+import org.apache.spark.ml.clustering.LocalLDAModel
+
+/**
+  * Created by chris on 8/18/17.
+  */
+object MPLDAModel {
+    def loadModel(path:String):LocalLDAModel = ???
+}
