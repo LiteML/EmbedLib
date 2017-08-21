08ad47bf8e4c46310fd0d43daad3d1c70444e770
chris <rihan.chen@tendcloud.com>
2017-08-21 11:13:01 +0800

    model

--- src/main/scala/embed/lda/spark/MPLDAModel.scala
+++ src/main/scala/embed/lda/spark/MPLDAModel.scala
@@ -1,10 +1,30 @@
 package embed.lda.spark
 
-import org.apache.spark.ml.clustering.LocalLDAModel
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.mllib.clustering.LocalLDAModel
+import org.apache.spark.mllib.linalg.{Matrices, Matrix, Vectors}
+
+import scala.collection.mutable
 
 /**
   * Created by chris on 8/18/17.
   */
 object MPLDAModel {
-    def loadModel(path:String):LocalLDAModel = ???
+    val sparkConf = new SparkConf()
+    val sc = new SparkContext(sparkConf)
+    //load the word count matrix to topic matrix
+    def wc2Tm(path:String): Matrix = ???
+    def loadModel(path:String): LocalLDAModel = {
+        val conf = sc.textFile(path+"/Configuration")
+        var confMap=new mutable.HashMap[String,String]()
+        conf.foreach(line=>{
+            confMap.put(line.split("=")(0).trim(),line.split("=")(1).trim())
+        })
+        val topicMatrix= sc.textFile(path+"/word_topic").flatMap(line=>{line.split(" ").map(_.toDouble)}).collect()
+        new LocalLDAModel(
+            Matrices.dense(confMap("vocabSize").toInt,confMap("k").toInt,topicMatrix),//topicMatrix
+            Vectors.dense(confMap.get("docConcentration").toString.split(",").map(_.toDouble)),//docConcentration
+            confMap("topicConcentration").toDouble//topicConcentration
+        )
+    }
 }
