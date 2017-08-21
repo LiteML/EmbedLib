efdb493a5b9fc9bd1a8fd2e4df591bfdf846bf45
chris <rihan.chen@tendcloud.com>
2017-08-21 19:35:04 +0800

    model

--- src/main/scala/embed/lda/spark/MPLDAModel.scala
+++ src/main/scala/embed/lda/spark/MPLDAModel.scala
@@ -13,14 +13,25 @@ object MPLDAModel {
     val sparkConf = new SparkConf()
     val sc = new SparkContext(sparkConf)
     //load the word count matrix to topic matrix
-    def wc2Tm(path:String): Array[Double] = ???
+    def wc2Tm(path:String,confMap:mutable.HashMap[String,String]): Array[Double] = {
+        val data=sc.textFile(s"$path/word_topic").map(_.split(" ").map(_.toInt))
+        val beta = confMap("topicConcentration").toDouble
+        val vocalSize=confMap("vocalSize").toDouble
+        val sum = data.treeReduce((a, b)=> Array.tabulate(confMap("k").toInt)(i => a(i) + b(i)))
+        val broadSum=sc.broadcast(sum)
+        data.flatMap(row=>{
+            val sum=broadSum.value
+            Array.tabulate(confMap("k").toInt)(i=> row(i) + beta /(sum(i) + beta*vocalSize))
+        }).collect()
+    }
+
     def loadModel(path:String): LocalLDAModel = {
         val conf = sc.textFile(path+"/Configuration")
         var confMap=new mutable.HashMap[String,String]()
         conf.foreach(line=>{
             confMap.put(line.split("=")(0).trim(),line.split("=")(1).trim())
         })
-        val topicMatrix= wc2Tm(path)
+        val topicMatrix= wc2Tm(path, confMap)
         new LocalLDAModel(
             Matrices.dense(confMap("vocabSize").toInt,confMap("k").toInt,topicMatrix),//topicMatrix
             Vectors.dense(confMap("docConcentration").split(",").map(_.toDouble)),//docConcentration
